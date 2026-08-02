8af48b94276193522c64e2e46a693bbaa1cc103a
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-02 15:28:06 +0700

    docs & feat: memperbaiki indentasi dan menyederhanakan tombol_angka

--- script.js
+++ script.js
@@ -1,141 +1,96 @@
-document.addEventListener("DOMContentLoaded", function(){
-    // Ambil elemen DOM
-    const tombol_angka = document.querySelectorAll('.btnNumber');
-    const display_angka = document.getElementById('display');
-    const tombol_hapus = document.getElementById('btnHapus');
-    const tombol_operator = document.querySelectorAll('.btnOperator');
-    const tombol_hasil = document.getElementById('btnHasil')
-    const tombol_clear = document.getElementById('clear')
+document.addEventListener("DOMContentLoaded", function () {
+  // Ambil elemen DOM
+  const tombol_angka = document.querySelectorAll(".btnNumber");
+  const display_angka = document.getElementById("display");
+  const tombol_hapus = document.getElementById("btnHapus");
+  const tombol_operator = document.querySelectorAll(".btnOperator");
+  const tombol_hasil = document.getElementById("btnHasil");
+  const tombol_clear = document.getElementById("clear");
 
-    // variabel kosong
-    let dataAngka = [[]];
-    let operator_terpilih = [];
-    let operatorDone = true;
+  // variabel kosong
+  let dataAngka = [[]];
+  let operator_terpilih = [];
+  let operatorDone = true;
 
-    // fungsi untuk memisahkan bilangan 1 dan bilangan 2
-    function inputNUM(value, save) {
-        let indexAkhir = save.length - 1;
-        return save[indexAkhir].push(value)
+  // fungsi untuk memisahkan bilangan 1 dan bilangan 2
+  function inputNUM(value, save) {
+    let indexAkhir = save.length - 1;
+    return save[indexAkhir].push(value);
+  }
+
+  function inputOP(save, operan, option) {
+    if (save[save.length - 1].length > 0) {
+      save.push([]);
+      operan.push(option);
     }
+  }
 
-    function inputOP(save,operan,option) {
-        save.push([]);
-        operan.push(option);
-        return save,operan
-    }
+  // event saat mengklik tombol angkaq
+  tombol_angka.forEach((tombol) => {
+    tombol.addEventListener("click", function () {
+      let teks = this.innerText;
 
-    // event saat mengklik tombol angkaq
-    tombol_angka.forEach(tombol => {
-        tombol.addEventListener('click', function(){
-            let teks = this.innerText;
-            switch (teks) {
-                case "1":
-                    display_angka.innerText += "1";
-                    inputNUM("1",dataAngka)
-                    break;
+      if (teks === "0") {
+        if (display_angka.trim === "") {
+            alert('jangan mengawali dengan 0');
+            return
+        }
+      }
 
-                case "2":
-                    display_angka.innerText += "2";
-                    inputNUM("2",dataAngka)
-                    break;
+      display_angka.innerText += teks;
+      inputNUM(teks,dataAngka)
+    });
+  });
 
-                case "3":
-                    display_angka.innerText += "3";
-                    inputNUM("3",dataAngka)
-                    break;
+  // event saat mengklik tombol hapus
+  tombol_hapus.addEventListener("click", function () {
+    let tekslama = display_angka.innerText;
+    let teksbaru = tekslama.slice(0, -1);
+    display_angka.innerText = teksbaru;
+  });
 
-                case "4":
-                    display_angka.innerText += "4";
-                    inputNUM("4",dataAngka)
-                    break;
+  // event saat mengklik tombol operator
+  tombol_operator.forEach((tombol) => {
+    tombol.addEventListener("click", function () {
+      let operator = tombol.innerText;
+      switch (operator) {
+        case "×":
+          display_angka.innerText += "×";
+          inputOP(dataAngka, operator_terpilih, "*");
+          break;
 
-                case "5":
-                    display_angka.innerText += "5";
-                    inputNUM("5",dataAngka)
-                    break;
+        case "+":
+          display_angka.innerText += "+";
+          inputOP(dataAngka, operator_terpilih, "+");
+          break;
 
-                case "6":
-                    display_angka.innerText += "6";
-                    inputNUM("6",dataAngka)
-                    break;
-                
-                case "7":
-                    display_angka.innerText += "7";
-                    inputNUM("7",dataAngka)
-                    break;
-                
-                case "8":
-                    display_angka.innerText += "8";
-                    inputNUM("8",dataAngka)
-                    break;
+        case "÷":
+          display_angka.innerText += "÷";
+          inputOP(dataAngka, operator_terpilih, "/");
+          break;
 
-                case "9":
-                    display_angka.innerText += "9";
-                    inputNUM("9",dataAngka)
-                    break;
+        case "-":
+          display_angka.innerText += "-";
+          inputOP(dataAngka, operator_terpilih, "-");
+          break;
+      }
+    });
+  });
 
-                case "0":
-                    if (display_angka.innerText.trim() === "") {
-                        alert('jangan mengawali dengan angka 0')
-                    } else {
-                        display_angka.innerText += "0";
-                        inputNUM("0",dataAngka)
-                    }
-                    break;
-            }
-        })
-    })
+  // event ketika mengklik tombol equal
+  tombol_hasil.addEventListener("click", function () {
+    let angkaTersimpan = [];
+    dataAngka.forEach((bilangan) => {
+      bilanganAkhir = Number(bilangan.join(""));
+      angkaTersimpan.push(bilanganAkhir);
+    });
 
-    // event saat mengklik tombol hapus
-    tombol_hapus.addEventListener("click", function(){
-        let tekslama = display_angka.innerText;
-        let teksbaru = tekslama.slice(0,-1);
-        display_angka.innerText = teksbaru
-    })
+    let ekspresi = angkaTersimpan.reduce((rumus, num, index) => {
+      const op = operator_terpilih[index - 1];
+      return `${rumus} ${op} ${num}`;
+    });
 
-    // event saat mengklik tombol operator
-    tombol_operator.forEach(tombol => {
-        tombol.addEventListener("click", function(){
-            let operator = tombol.innerText
-            switch (operator) {
-                case "×":
-                    display_angka.innerText += "×"
-                    inputOP(dataAngka,operator_terpilih,"*")
-                    break;
-                    
-                    
-                case "+":
-                    display_angka.innerText += "+"
-                    inputOP(dataAngka,operator_terpilih,"+")
-                    break;
-
-                case "÷":
-                    display_angka.innerText += "÷"
-                    inputOP(dataAngka,operator_terpilih,"/")
-                    break;
-
-                case "-":
-                    display_angka.innerText += "-"
-                    inputOP(dataAngka,operator_terpilih,"-")
-                    break;
-            }
-        })
-    })
-
-    // event ketika mengklik tombol equal
-    tombol_hasil.addEventListener("click",function(){
-        let angkaTersimpan = []
-        dataAngka.forEach((bilangan) => {
-            bilanganAkhir = Number(bilangan.join(""));
-            angkaTersimpan.push(bilanganAkhir)
-        })
-        
-        let ekspresi = angkaTersimpan.reduce((rumus,num,index) => {
-            const op = operator_terpilih[index - 1];
-            return `${rumus} ${op} ${num}`;
-        })
-
-        let hasilAkhir = eval(ekspresi);
-        display_angka.textContent = hasilAkhir
-    })
-})
+    let hasilAkhir = eval(ekspresi);
+    display_angka.textContent = hasilAkhir;
+  });
+});
