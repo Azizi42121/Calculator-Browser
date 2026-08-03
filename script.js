24bfd0eddaf8d4d71e1c3b7ac556fcdf6057936c
Rojul Jundan Azizi <Rojulazizi@gmail.com>
2026-08-03 12:57:16 +0700

    feat: memperbaiki tampilan display_angka dan display_op

--- script.js
+++ script.js
@@ -72,22 +72,22 @@ document.addEventListener("DOMContentLoaded", function () {
       let operator = tombol.innerText;
       switch (operator) {
         case "×":
-          display_angka.innerText += " × ";
+          display_op.textContent = " × ";
           inputOP(dataAngka, operator_terpilih, "*");
           break;
 
         case "+":
-          display_angka.innerText += " + ";
+          display_op.textContent = " + ";
           inputOP(dataAngka, operator_terpilih, "+");
           break;
 
         case "÷":
-          display_angka.innerText += " ÷ ";
+          display_op.textContent = " ÷ ";
           inputOP(dataAngka, operator_terpilih, "/");
           break;
 
         case "-":
-          display_angka.innerText += " - ";
+          display_op.textContent = " - ";
           inputOP(dataAngka, operator_terpilih, "-");
           break;
       }
